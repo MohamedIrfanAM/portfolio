4c271d2167688e74bd229e03a1eb5a7e95c173f3
Mohamed Irfan <i4irfan777@gmail.com>
2023-08-17 14:35:06 +0530

    typing subheading animation on landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,14 @@ import { intro, socialLinks } from "@/constants"
 import styles, {fadeIn } from "@/app/style"
 import {BsBoxArrowInUpRight} from "react-icons/bs"
 import { motion } from "framer-motion"
+import { useTypewriter,Cursor } from "react-simple-typewriter"
 
 const dancing_script = Dancing_Script({
   subsets:['latin'],
 })
 
 const Hero = () => {
+  const [subheading] = useTypewriter({words:['Software Developer','CS Student','Tech Enthusiast'],loop:{},typeSpeed:60,deleteSpeed:80})
   return (
     <section className="h-screen w-full p-1" id='hero'>
       <div className={`text-2xl sm:text-3xl font-bold ${dancing_script.className} ${styles.text_gradient}`}>@irfan_</div>
@@ -24,13 +26,13 @@ const Hero = () => {
         <div className="flex flex-col sm:pl-[120px] sm:pt-4">
           <motion.div className="text-center sm:text-left" variants={fadeIn("right","spring",0.1,0.75)} initial='hidden' whileInView='visible'>
             <div className="text-cyan-400 text-[28px] font-bold sm:text-[45px]">Hi, I'm Mohamed Irfan</div>
-            <div className="font-bold text-primary text-xl sm:text-2xl">Software Developer</div>
+            <div className="font-bold text-primary text-xl sm:text-2xl"><span>{subheading}<Cursor/></span></div>
             <div className="max-w-[300px] mt-1 text-secondary sm:text-md sm:mt-4 sm:max-w-[500px]">{intro}</div>
           </motion.div>
-          <motion.div className="mx-auto mt-5 sm:mx-0 sm:mt-7 flex justify-start gap-4" variants={fadeIn("left",'spring',0.5,0.75)} initial='hidden' whileInView='visible'>
+          <motion.div className="mx-auto mt-5 sm:mx-0 sm:mt-7 flex justify-start gap-4 group" variants={fadeIn("left",'spring',0.5,0.75)} initial='hidden' whileInView='visible'>
             <Link href='https://drive.google.com/file/d/1-7s7WCv4Hf7OD_agK9RHYO3tRmkKeRsW/view?usp=sharing' target="_blank" className="text-dark bg-gradient-to-r from-primary to bg-cyan-400 hover:scale-110 px-4 py-2 flex justify-between rounded">
               Resume
-              <BsBoxArrowInUpRight className="ml-1 mt-1"/>
+              <BsBoxArrowInUpRight className="ml-1 mt-1 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"/>
             </Link>
             <Lk to='contact' smooth={true} spy={true}>
               <button class="text-primary bg-dark border-primary py-2 px-4 rounded border hover:scale-110">
