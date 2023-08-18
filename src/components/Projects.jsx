073e2df8620c45d67b20881c54a8a3299c6b4fde
Mohamed Irfan <i4irfan777@gmail.com>
2023-08-18 20:48:47 +0530

    fix animation overflow from projects to the sides

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,8 +7,8 @@ import { motion } from "framer-motion"
 
 const Projects = () => {
   return (
-    <section className="w-full py-14 lg:pr-24 h-screen " id='projects'>
-      <motion.h1 className={`text-center text-4xl font-bold mx-10 relative left-2 ${styles.text_gradient}`} variants={fadeIn("left","spring",0.2,0.75)} initial='hidden' whileInView='visible'>Recent Projects</motion.h1>
+    <section className="w-screen overflow-hidden py-14 lg:pr-24 h-screen " id='projects'>
+      <motion.h1 className={`text-center text-4xl font-bold mx-10 relative left-2 bg-clip-text text-transparent bg-gradient-to-r from-primary to-cyan-400`} variants={fadeIn("left","spring",0.2,0.75)} initial='hidden' whileInView='visible'>Recent Projects</motion.h1>
 
       <motion.div variants={fadeIn("right","spring",0.2,0.75)} initial='hidden' whileInView='visible'>
         <ProjectsSlider/>
